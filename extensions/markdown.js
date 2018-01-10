76e7301328ef885a76a57f91ff75236e96e8f7c6
uhyo <uhyo@uhy.ooo>
2018-01-11 01:05:23 +0900

    Initial commit

---
+++ extensions/markdown.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const md = require('markdown-it')();
+const pug = require('pug');
+
+module.exports = (context)=>{
+    context.addUnknownExtensionHook((context, ext)=>{
+        if (ext === '.md') {
+            return makeMarkdownRenderFunction(context);
+        }
+    });
+};
+
+/**
+ * Make a markdown render function.
+ */
+function makeMarkdownRenderFunction(ctx){
+    // Compile a pug template.
+    const templateFunc = pug.compileFile(
+        path.join(__dirname, '../templates/main.pug'),
+        {
+        });
+
+   
+    return markdownRenderFunction;
+   
+    function markdownRenderFunction(file, outDir, options){
+        const target = ctx.getTargetFile(file, outDir);
+
+        return ctx.render(file, target, ()=>{
+            return ctx.loadRenderedFile(file, false).then(content=>{
+                return Promise.resolve(templateFunc({
+                    content: md.render(content),
+                }));
+            });
+        });
+    }
+}
